c580dc382d505fc6f593a49a20361e39c97c37ee
Ch-franck <Ch-franck@2823377353@qq.com>
2023-07-01 17:12:42 +0800

    用户项

--- Backend/LiveSys/models.py
+++ Backend/LiveSys/models.py
@@ -5,7 +5,7 @@ class Liveroom(models.Model):
     roomID = models.AutoField(verbose_name="roomID")
     roomName = models.CharField(verbose_name="roomName")
     roomInfo = models.CharField(verbose_name="roomInfo")
-    # Owner = models.ForeignKey(verbose_name="Owner")
+    Owner = models.ForeignKey(on_delete=models.CASCADE,verbose_name="Owner")
 
     class Meta:
         db_table = "直播间表"
@@ -13,8 +13,8 @@ class Liveroom(models.Model):
 
 class RelationbUandL(models.Model):
     RID = models.AutoField(verbose_name="RID")
-    # FUser = models.ForeignKey(verbose_name="FUser")
-    FLiveroom = models.ForeignKey(Liveroom,verbose_name="FLiveroom")
+    FUser = models.ForeignKey(on_delete=models.CASCADE,verbose_name="FUser")
+    FLiveroom = models.ForeignKey(Liveroom,on_delete=models.CASCADE,verbose_name="FLiveroom")
     isManager = models.BooleanField(default=False,verbose_name="FLiveroom")
     isProhibited = models.BooleanField(default=False,verbose_name="FLiveroom")
 
