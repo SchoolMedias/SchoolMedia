cee8ea35dd95b48d821324769a21601db82cfeca
Ch-franck <Ch-franck@2823377353@qq.com>
2023-07-01 17:09:26 +0800

    缺少用户项

--- Backend/MeetingSys/models.py
+++ Backend/MeetingSys/models.py
@@ -1,22 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Liveroom(models.Model):
-    roomID = models.AutoField(verbose_name="roomID")
-    roomName = models.CharField(verbose_name="roomName")
-    roomInfo = models.CharField(verbose_name="roomInfo")
-    Owner = models.ForeignKey(verbose_name="Owner")
-
-    class Meta:
-        db_table = "直播间表"
-
-
-class RelationbUandL(models.Model):
-    RID = models.AutoField(verbose_name="RID")
-    # FUser = models.ForeignKey(verbose_name="FUser")
-    FLiveroom = models.ForeignKey(Liveroom,verbose_name="FLiveroom")
-    isManager = models.BooleanField(default=False,verbose_name="FLiveroom")
-    isProhibited = models.BooleanField(default=False,verbose_name="FLiveroom")
-
-    class Meta:
-        db_table = "用户与直播间关系表"
